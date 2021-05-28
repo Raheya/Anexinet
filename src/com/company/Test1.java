ba03d95587eabc1b8076937d29cace1d23dcbb57
Carlos  Torres <carlos.torres@digitalonus.com>
2021-05-27 21:09:22 -0500

    test 1 final effort

--- src/com/company/Test1.java
+++ src/com/company/Test1.java
@@ -7,9 +7,14 @@ import java.util.List;
 public class Test1 {
     public static void main(String[] args) {
         List<Integer> leapYears = new ArrayList<>();
+        int times =0;
         for (int i = 2021; i < 2150 ; i++) {
-            if ((i % 4 == 0) && ((i % 100 != 0) || (i % 400 == 0)))
-                leapYears.add(i);
+            if(times<20) {
+                if ((i % 4 == 0) && ((i % 100 != 0) || (i % 400 == 0))) {
+                    leapYears.add(i);
+                    times++;
+                }
+            }
         }
 
         System.out.println(Arrays.toString(leapYears.toArray()));
